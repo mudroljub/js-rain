fcb83afd15faa33f58fd10bdc911a5287f8449e1
Damjan <dpavlica@itekako.com>
2016-04-08 18:51:33 +0200

    razdvojene klase

--- klase/Kap.js
+++ klase/Kap.js
@@ -1,11 +1,8 @@
+import {Prasak} from './Prasak'
+
 const VISINA_KAPI = 9;
 const BRZINA_KISHE = 2.8;
 const TRAJANJE_PRASKA = 100;
-const MAX_DOMET_PRASKA = 30;
-const BROJ_PRSKANJA = 3;
-const BRZINA_PRSKANJA = 1.5;
-const VELICINA_BARICE = 3;
-const BRZINA_BARICE = 0.1;
 
 export class Kap {
 
@@ -43,55 +40,3 @@ export class Kap {
   }
 
 } // Kap
-
-class Prasak {
-
-  constructor(podloga, x, y) {
-    this.podloga = podloga;
-    this.x = x;
-    this.y = y;
-    this.raspon = 0;
-    this.ugloviPrskanja = [];
-  }
-
-  update() {
-    if (!this.x || !this.y) return;
-    this.y += BRZINA_BARICE;
-    this.raspon += BRZINA_PRSKANJA;
-  }
-
-  reset() {
-    this.x = null;
-    this.y = null;
-  }
-
-  crtaBaricu() {
-    if (!this.x || !this.y) return;
-    this.podloga.beginPath();
-    this.podloga.arc(this.x, this.y, VELICINA_BARICE, 0, 2 * Math.PI);
-    this.podloga.fill();
-    this.podloga.stroke();
-  }
-
-  crtaPrskanje() {
-    if (!this.x || !this.y || this.raspon > MAX_DOMET_PRASKA) return;
-    for (var i = 0; i < BROJ_PRSKANJA; i++) {
-      let randomUgao = randomInRange(1, 2) * Math.PI;
-      this.ugloviPrskanja[i] = this.ugloviPrskanja[i] || randomUgao;
-      this.podloga.beginPath();
-      this.podloga.arc(this.x, this.y, this.raspon, this.ugloviPrskanja[i], this.ugloviPrskanja[i] + 0.1);
-      this.podloga.stroke();
-    }
-  }
-
-  crta() {
-    this.crtaBaricu();
-    this.crtaPrskanje();
-  }
-
-} // Prasak
-
-
-function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-}
